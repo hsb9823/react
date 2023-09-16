099aa09eacfc4d7529b3ed9a61e6705c5062be76
hsb9823 <hsb3849@naver.com>
2023-09-17 01:22:44 +0900

    ReactMovieWEB

--- src/movieApp.js
+++ src/movieApp.js
@@ -6,7 +6,10 @@ function MovieApp() {
     <Router>
       <Routes>
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
-        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        ></Route>
       </Routes>
     </Router>
   );
