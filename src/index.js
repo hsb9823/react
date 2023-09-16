771d55753d3fba05bccb5e260fbbc75e65b95d64
hsb9823 <hsb3849@naver.com>
2023-09-17 01:12:39 +0900

    ReactMovieWeb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Todo from "./Todo";
 import Coin from "./cointracker";
 import MovieApp from "./movieApp";
 ReactDOM.render(<MovieApp />, document.getElementById("root"));
