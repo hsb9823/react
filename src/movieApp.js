dd1dc7c82e3583d7e712cd312fb49fe000dd27a4
hsb9823 <hsb3849@naver.com>
2023-09-17 01:55:45 +0900

    reactPractice

--- src/movieApp.js
+++ src/movieApp.js
@@ -3,7 +3,7 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 function MovieApp() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/movie/:id" element={<Detail />}></Route>
