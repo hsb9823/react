2e663b0a43506f8e0f47a1683ead9129718e2419
hsb9823 <hsb3849@naver.com>
2023-09-17 01:52:20 +0900

    reactPractice

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import Movie from "./components/Movie";
 import MovieApp from "./movieApp";
 
-import { BrowserRouter } from "react-router-dom";
-<BrowserRouter basename={process.env.PUBLIC_URL}>
-  <MovieApp />
-</BrowserRouter>;
+// import { BrowserRouter } from "react-router-dom";
+// <BrowserRouter basename={process.env.PUBLIC_URL}>
+//   <MovieApp />
+// </BrowserRouter>;
 ReactDOM.render(<MovieApp />, document.getElementById("root"));
