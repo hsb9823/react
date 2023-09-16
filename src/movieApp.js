0f34505c65af3ab945e974f1dd7b1f6a60d5be5d
hsb9823 <hsb3849@naver.com>
2023-09-17 01:35:49 +0900

    reactPractice

--- src/movieApp.js
+++ src/movieApp.js
@@ -5,8 +5,13 @@ function MovieApp() {
   return (
     <Router>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
         <Route
+          basename={process.env.PUBLIC_URL}
+          path={`${process.env.PUBLIC_URL}/`}
+          element={<Home />}
+        ></Route>
+        <Route
+          basename={process.env.PUBLIC_URL}
           path={`${process.env.PUBLIC_URL}/movie/:id`}
           element={<Detail />}
         ></Route>
