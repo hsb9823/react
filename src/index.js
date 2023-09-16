6e6688cb51427291f8982beb537501d35428d243
hsb9823 <hsb3849@naver.com>
2023-09-17 01:19:03 +0900

    ReactMovieWEB

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Movie from "./components/Movie";
 import MovieApp from "./movieApp";
-import { BrowserRouter } from "react-router-dom";
-<BrowserRouter basename="process.env.PUBLIC_URL">
-  <MovieApp />
-</BrowserRouter>;
+
 ReactDOM.render(<MovieApp />, document.getElementById("root"));
